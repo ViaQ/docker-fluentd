a4c76e6b897d139ecad93e20a069c77ae0c9ee76
Lukáš Vlček <lukas.vlcek@gmail.com>
2016-06-07 17:31:20 +0200

    Add basic travis support.

---
+++ travis_scripts/basic_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This is the most basic test. If container did not start correctly then print out logs and exit.
+
+set -ev
+
+# Give the container some room to start up
+sleep 10
+docker ps -a
+
+CONTAINER_ID=$(docker ps -aq)
+STATUS=$(docker inspect -f {{.State.Running}} $CONTAINER_ID)
+
+if [ "$STATUS" == "false" ]; then
+  echo "CRITICAL - $CONTAINER_ID is not running. Check logs:"
+  docker logs $CONTAINER_ID
+  exit 2
+fi
